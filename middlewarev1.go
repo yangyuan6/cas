2f34a5329fe5cd1b15f62cbfb1948de5e4367fbd
yang yuan <yuan.yang@bitmain.com>
2021-06-16 11:05:52 +0800

    解决前后端分离，从前端跳转问题

--- middlewarev1.go
+++ middlewarev1.go
@@ -23,25 +23,8 @@ func (c *Client) HandlerV1(h http.Handler) http.Handler {
 		setClient(r, c)
 
 		if !IsAuthenticated(r) {
-			//c := getClient(r)
-			//if c == nil {
-			//	err := "cas: redirect to cas failed as no client associated with request"
-			//	http.Error(w, err, http.StatusInternalServerError)
-			//	return
-			//}
-			//u, err := c.LoginUrlForRequest(r)
-			//if err != nil {
-			//	http.Error(w, err.Error(), http.StatusInternalServerError)
-			//	return
-			//}
-			//
-			//if glog.V(2) {
-			//	glog.Infof("Redirecting client to %v with status %v", u, http.StatusFound)
-			//}
-			//RedirectToLogin(w, r)
 			genRedirectUrl := fmt.Sprintf("%s/login?service=%s", RedirectUrl, url.QueryEscape("http://"+r.Host+LoginUri))
 			render.JSON(w, r, map[string]string{"code": strconv.Itoa(REDIRECT_CODE), "data": genRedirectUrl, "msg": "redirect login."})
-
 			return
 		}
 
